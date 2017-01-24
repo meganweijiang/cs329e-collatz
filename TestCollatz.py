c0a464d3e011f81bf33c02dc27002efd729252ed
Megan Weijiang <meganweijiang@utexas.edu>
2017-01-24 17:32:31 -0600

    Add more unit tests to TestCollatz.py
    Average of 3 unit tests per method

--- TestCollatz.py
+++ TestCollatz.py
@@ -27,11 +27,23 @@ class TestCollatz (TestCase):
     # read
     # ----
 
-    def test_read(self):
+    def test_read_1(self):
         s = "1 10\n"
         i, j = collatz_read(s)
         self.assertEqual(i,  1)
         self.assertEqual(j, 10)
+
+    def test_read_2(self):
+        s = "100 1000\n"
+        i, j = collatz_read(s)
+        self.assertEqual(i,  100)
+        self.assertEqual(j, 1000)
+
+    def test_read_3(self):
+        s = "2559 4598\n"
+        i, j = collatz_read(s)
+        self.assertEqual(i, 2559)
+        self.assertEqual(j, 4598)
 
     # ----
     # eval
@@ -57,21 +69,45 @@ class TestCollatz (TestCase):
     # print
     # -----
 
-    def test_print(self):
+    def test_print_1(self):
         w = StringIO()
         collatz_print(w, 1, 10, 20)
         self.assertEqual(w.getvalue(), "1 10 20\n")
+
+    def test_print_2(self):
+        w = StringIO()
+        collatz_print(w, 100, 200, 125)
+        self.assertEqual(w.getvalue(), "100 200 125\n")
+
+    def test_print_3(self):
+        w = StringIO()
+        collatz_print(w, 900, 1000, 174)
+        self.assertEqual(w.getvalue(), "900 1000 174\n")
 
     # -----
     # solve
     # -----
 
-    def test_solve(self):
+    def test_solve_1(self):
         r = StringIO("1 10\n100 200\n201 210\n900 1000\n")
         w = StringIO()
         collatz_solve(r, w)
         self.assertEqual(
             w.getvalue(), "1 10 20\n100 200 125\n201 210 89\n900 1000 174\n")
+
+    def test_solve_2(self):
+        r = StringIO("50 100\n555 777\n800 2000\n2999 3500\n")
+        w = StringIO()
+        collatz_solve(r, w)
+        self.assertEqual(
+            w.getvalue(), "50 100 119\n555 777 171\n800 2000 182\n2999 3500 199\n")
+
+    def test_solve_3(self):
+        r = StringIO("5 6\n2000 2005\n880 900\n8 999\n")
+        w = StringIO()
+        collatz_solve(r, w)
+        self.assertEqual(
+            w.getvalue(), "5 6 9\n2000 2005 144\n880 900 148\n8 999 179\n")
 
 # ----
 # main
